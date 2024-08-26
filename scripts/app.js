ab698f344f290da7ae397a6f7754b7b7ff8dd32d
Keyaanna (Kiko) Pausch <knpausch@gmail.com>
2024-08-26 15:35:57 -0700

    reorganize code

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,9 @@
 
   const tweetContainer = document.getElementById("tweet-container");
 
-  Squirrel.addEventListener("eventDispatch", (e) => eval(`${e.detail.name}(e)`));
+  Squirrel.addEventListener("eventDispatch", (e) =>
+    eval(`${e.detail.name}(e)`)
+  );
   Squirrel.initWithSquirrel();
 
   function onInitState(e) {
@@ -39,17 +41,21 @@
   }
 
   function renderTweet() {
-    const proxyUrl = "http://localhost:3000/";
-    const apiUrl = `${proxyUrl}https://publish.twitter.com/oembed?url=${encodeURIComponent(twitterPostURL)}`;
+    document.addEventListener("DOMContentLoaded", () => {
+      const tweetUrl = "https://x.com/Squirrel_365/status/1813136575007433144";
+      const proxyUrl = "http://localhost:3000/";
+      const apiUrl = `${proxyUrl}https://publish.twitter.com/oembed?url=${encodeURIComponent(tweetUrl)}`;
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        tweetContainer.innerHTML = data.html; 
-      })
-      .catch((error) => {
-        console.error("Error fetching tweet:", error);
-      });
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.html); 
+          tweetContainer.innerHTML = data.html; 
+        })
+        .catch((error) => {
+          console.error("Error fetching tweet:", error); 
+        });
+    });
   }
 
   function onPropertyChangesComplete() {}
