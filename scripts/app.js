3d13046d1477e64a5028f11439620268fa5ba350
Keyaanna (Kiko) Pausch <knpausch@gmail.com>
2024-08-26 14:49:08 -0700

    set global variables widgetType and twitterPostURL initialize then update variable when user selects/enters data via propery panel

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,6 @@
       console.log("STATE: ", state);
       twitterPostURL = state.postURL;
       widgetType = state.styleType;
-      console.log("WIDGET: ", widgetType);
     }
   }
 
@@ -21,9 +20,10 @@
 
     switch (Squirrel.getGenericProperty(propertyName)) {
       case "styleType":
-        console.log("you clicked ", propertyName);
         widgetType = propertyValue;
-        console.log("WIDGET: ", widgetType);
+        break;
+      case "postURL":
+        twitterPostURL = propertyValue;
         break;
       default:
         console.log("Unknown message type: " + propertyName);
@@ -31,47 +31,26 @@
     }
   }
 
-  // listen to Squirrel lifecycle events
   Squirrel.addEventListener("eventDispatch", (e) =>
     eval(`${e.detail.name}(e)`)
   );
 
-  // initialise
   Squirrel.initWithSquirrel();
-
-  /**
-   * Take a string, to be shown in the HTML display, capitalise
-   * the letters and then send back to Squirrel
-   * @param value The string to display and return
-   */
-  function processData(value) {
-    document.getElementById("helloWorldText").textContent = value;
-    Squirrel.sendToSquirrel("helloWorldResponse", value.toUpperCase());
-  }
-
-  /**
-   * Called at the end of a series of property value changes.  This can be called
-   * either when a single or multiple values change at once.  This is the flag to say
-   * There are no more incoming value changs to process at this time.
-   */
+  
   function onPropertyChangesComplete() {}
 
-  // Called when a setCanvas event is received from Squirrel
   function onSetCanvas(e) {
     const canvas = e.detail.canvas;
   }
 
-  // Called when a setRuntimeMode event is received from Squirrel
   function onSetRuntimeMode(e) {
     const mode = e.detail.mode;
   }
 
-  // Called when a setSize event is received from Squirrel
   function onSetSize(e) {
     const size = e.detail.size;
   }
 
-  // Called when a setPosition event is received from Squirrel
   function onSetPosition(e) {
     const position = e.detail.position;
   }
